07844a00700ee38af31362cc4e633c44e929ff95
mikeohl <mikeohl@live.com>
2018-01-11 05:47:57 -0800

    Added methods to raise, lower and renew pins; refactored other methods

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -7,36 +7,106 @@ public class PinSetter : MonoBehaviour {
 
     // public Pin [] pins;
     public Text standingDisplay;
+    public int standingCount = -1;
+    public float resetTime = 4.0f;
+    public float pinRaiseHeight = 40.0f;
+    public GameObject pins;
 
+    private Ball ball;
+    private float standingCountUpdateTime;
     private bool ballEnteredBox = false;
+    private float pinsPos;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ball = GameObject.FindObjectOfType<Ball>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        standingDisplay.text = CountStanding().ToString();
+        if (ballEnteredBox)
+        {
+            UpdateStandingCount();
+            if (Time.time - standingCountUpdateTime >= resetTime)
+            {
+                ResetBall();
+            }
+        }
 	}
 
-    public int CountStanding ()
+    // Count the number of upright pins
+    public int CountStandingPins ()
     {
-        // Assume all pins are standing
-        int standingPins = 10;
+        // Assume NO pins are standing
+        int standingPins = 0;
 
         // Loop through pins, check if standing and update count
         foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
         {
-            if (!pin.IsStanding())
+            if (pin.IsStanding())
             {
-                standingPins--;
+                standingPins++;
             }
         }
         return standingPins;
     }
 
-    public void OnTriggerEnter (Collider collider)
+    // Update the standing count and last updated time if standingCount has changed
+    private void UpdateStandingCount ()
+    {
+        int currentCount = CountStandingPins();
+
+        if (standingCount != currentCount)
+        {
+            standingCount = currentCount;
+            standingCountUpdateTime = Time.time;
+            standingDisplay.text = currentCount.ToString();
+        }
+    }
+
+    // Reset lastStandingCount, ballEnteredBox, display color
+    private void ResetBall ()
+    {
+        ball.Reset();
+        standingCount = -1;
+        ballEnteredBox = false;
+        standingDisplay.color = Color.black;
+    }
+
+    // Raise Pins to avoid swiper
+    public void RaisePins ()
+    {
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+        {
+            if (pin.IsStanding())
+            {
+                pin.transform.Translate(new Vector3(0.0f, pinRaiseHeight, 0.0f), Space.World);
+                pin.DisableGravity();
+                pin.Stabilize();
+            }
+        }
+    }
+
+    // Lower pins back down to the lane
+    public void LowerPins()
+    {
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+        {
+            pin.transform.Translate(new Vector3(0.0f, -pinRaiseHeight, 0.0f), Space.World);
+            pin.EnableGravity();
+            pin.Stabilize();
+        }
+    }
+
+    // Create a new set of 10 pins to be placed on the lane
+    public void RenewPins ()
+    {
+        Instantiate(pins);
+        RaisePins();
+    }
+
+
+    public void OnTriggerEnter(Collider collider)
     {
         GameObject triggeredObject = collider.gameObject;
 
@@ -44,13 +114,18 @@ public class PinSetter : MonoBehaviour {
         if (triggeredObject.GetComponent<Ball>())
         {
             ballEnteredBox = true;
-            UpdateColor();
-        }  
+            standingDisplay.color = Color.red;
+        }
     }
 
+    // Eliminate Pins that leave the Pin Setter box
     public void OnTriggerExit(Collider collider)
     {
+        // Attempt to get Pin object from collider
         Pin pin = collider.gameObject.GetComponentInParent<Pin>();
+
+        // If colliding object was pin, destroy child objects and then the pin itself.
+        // Otherwise, destroy the gameobject of the collider.
         if (pin)
         {
             foreach (Transform child in pin.transform)
@@ -58,17 +133,10 @@ public class PinSetter : MonoBehaviour {
                 Destroy(child.gameObject);
             }
             Destroy(pin.gameObject);
-        } else
-        {
-            Destroy(collider.gameObject);
         }
-        
-        //print(collider.gameObject.GetComponentInParent<Pin>());
-        //Destroy(collider.gameObject.GetComponentInParent<Pin>());
-    }
-
-    public void UpdateColor ()
-    {
-        standingDisplay.color = Color.red;
+        else
+        {
+            //Destroy(collider.gameObject);
+        }
     }
 }
