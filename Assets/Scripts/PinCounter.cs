3939d90e1f42f7ea29cf27c9d5e5d844598df814
mikeohl <mikeohl@live.com>
2018-01-15 06:00:31 -0800

    Refactored pin counting code from PinSetter to PinCounter

---
+++ Assets/Scripts/PinCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PinCounter : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // Count the number of upright pins
+    public int CountStandingPins()
+    {
+        // Assume NO pins are standing
+        int standingPins = 0;
+
+        // Loop through pins, check if standing and update count
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+        {
+            if (pin.IsStanding())
+            {
+                standingPins++;
+            }
+        }
+        return standingPins;
+    }
+}
