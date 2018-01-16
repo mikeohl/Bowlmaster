9c00dd16b28bcf750432a8648e81e424d760f5e0
mikeohl <mikeohl@live.com>
2018-01-16 07:21:43 -0800

    Script that has a triggerExit collider to detect ball leaving playspace

---
+++ Assets/Scripts/LaneBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneBox : MonoBehaviour {
+
+    private GameManager gameManager;
+    private bool ballOutOfPlay = false;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.name == "Ball")
+        {
+            gameManager.SetBallOutOfPlay(true);
+        }
+    }
+}
