7db6ec2e96e00c952eee234495b15d565acea076
mikeohl <mikeohl@live.com>
2018-01-17 06:12:01 -0800

    Minor refactor

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -2,35 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreMaster {
+public static class ScoreMaster {
 
-    // Returns a list of individual frame scores, NOT cumulative.
-    public static List<int> ScoreFrames (List<int> rolls)
+    // Returns a list of individual frame scores
+    public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
         Queue<int> memory = new Queue<int>();
-        
+
         foreach (int roll in rolls)
         {
             memory.Enqueue(roll);
-            if (memory.Peek() < 10 && memory.Count == 2 && roll < 10) 
+            if (memory.Count == 3) 
             {
-                if (memory.Peek() + roll < 10)
+                if (memory.Peek() == 10) // We have a STRIKE to be scored
                 {
-                    frameList.Add(memory.Peek() + roll);
-                    memory.Clear();
+                    frameList.Add(memory.Dequeue() + memory.Peek() + roll);
                 }
-            } else if (memory.Peek() < 10 && memory.Count == 3) // We have a spare to be scored
-            {
-                frameList.Add(memory.Dequeue() + memory.Dequeue() + memory.Peek());
-            } else if (memory.Peek() == 10 && memory.Count == 3) // We have a strike to be scored
-            {
-                frameList.Add(memory.Dequeue() + memory.Peek() + roll);
-                if (memory.Peek() + roll < 10 && frameList.Count != 10)
+                else // We have a SPARE to be scored
                 {
-                    frameList.Add(memory.Peek() + roll);
-                    memory.Clear();
-                }
+                    frameList.Add(memory.Dequeue() + memory.Dequeue() + memory.Peek());
+                } 
+            }
+            if (memory.Count == 2 && memory.Peek() + roll < 10 && frameList.Count < 10) // We have an OPEN frame to be scored
+            {
+                frameList.Add(memory.Peek() + roll);
+                memory.Clear();
             }
         }
 
