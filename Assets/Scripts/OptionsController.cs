3100e199c791b32dc29883a37335ad7df1277b03
mikeohl <mikeohl@live.com>
2018-03-05 02:15:28 -0800

    Added option controller to set and save ball color options

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,26 @@
+﻿/* OptionsController manages and persistently saves ball
+ * color in the options menu. 
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+    public Slider ballColorSlider;
+    public LevelManager levelManager;
+    
+    private string ballColor;
+    
+    // Use this for initialization
+    void Start () {
+        ballColor = PlayerPrefsManager.GetBallColor();
+        
+    }
+    
+    // Save options from the option menu to persistent storage
+    public void SaveAndExit () {
+        PlayerPrefsManager.SetBallColor (ballColor);
+        levelManager.LoadLevel ("01a Start Menu");
+    }
+}
