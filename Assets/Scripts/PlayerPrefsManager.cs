e60c8e2a987626e268c1729ca08366a96801aab8
mikeohl <mikeohl@live.com>
2018-03-07 06:01:03 -0800

    Script saves last score, high score and ball color to player prefs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,44 @@
+﻿/* PlayerPrefsManager saves High Score, Last Score,
+ * and ball color in Unity PlayerPrefs */
+
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string HIGH_SCORE_KEY = "high_score";
+    const string LAST_SCORE_KEY = "last_score";
+    const string BALL_COLOR_KEY = "ball_color";
+    
+    public static void SetHighScore (int score) {
+        if (score >= 0 && score <= 300) {
+            PlayerPrefs.SetInt (HIGH_SCORE_KEY, score);
+        } else {
+            Debug.LogError ("Score out of range");
+        }
+    }
+    
+    public static int GetHighScore () {
+        return PlayerPrefs.GetInt (HIGH_SCORE_KEY);
+    }
+
+    public static void SetLastScore(int score) {
+        if (score >= 0 && score <= 300) {
+            PlayerPrefs.SetInt(LAST_SCORE_KEY, score);
+        }
+        else {
+            Debug.LogError("Score out of range");
+        }
+    }
+
+    public static int GetLastScore () {
+        return PlayerPrefs.GetInt (LAST_SCORE_KEY);
+    }
+
+    public static void SetBallColor(string color){
+        PlayerPrefs.SetString(BALL_COLOR_KEY, color);
+    }
+
+    public static string GetBallColor() {
+        return PlayerPrefs.GetString(BALL_COLOR_KEY);
+    }
+}
