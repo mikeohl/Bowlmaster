270dab1ee412b2075fdd12d2813984f0eddd4e32
mikeohl <mikeohl@live.com>
2018-03-07 05:56:07 -0800

    Added comments

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿/* Camera control moves camera with ball down the lane until ball reaches pins */
+
+using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
@@ -11,13 +13,11 @@ public class CameraControl : MonoBehaviour {
     void Start () {
         offset = ball.transform.position - transform.position;
     }
-    
-    // Update is called once per frame
-    void Update () {
 
-        // Move the Camera with the ball until it gets to a certain point before the pins
-        if (ball.transform.position.z < cameraStopPosition)
-        {
+    // Update is called once per frame
+    // Move the Camera with the ball until it gets to a certain point before the pins
+    void Update () {     
+        if (ball.transform.position.z < cameraStopPosition) {
             transform.position = ball.transform.position - offset;
         }
     }
