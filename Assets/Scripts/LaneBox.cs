465bd3faa6b3ce62b827aa33a72be75cfe1f630c
mikeohl <mikeohl@live.com>
2018-03-07 05:58:27 -0800

    Added comments

--- Assets/Scripts/LaneBox.cs
+++ Assets/Scripts/LaneBox.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿/* LaneBox detects if the ball exits the lane box and calls
+ * the game manager to set it's out of play flag 
+ */
+
+using UnityEngine;
 
 public class LaneBox : MonoBehaviour {
 
@@ -9,6 +13,7 @@ public class LaneBox : MonoBehaviour {
         gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
+    // Set gameManager out of play flag on ball exit from lane box
     private void OnTriggerExit(Collider collider) {
         if (collider.gameObject.name == "Ball") {
             gameManager.SetBallOutOfPlay(true);
