8a70a1d76a950949616484dec3caa5cd3c8b80be
mikeohl <mikeohl@live.com>
2018-03-07 05:57:42 -0800

    Script sets UI high score and last score for end screen

---
+++ Assets/Scripts/EndScoreDisplay.cs
@@ -0,0 +1,20 @@
+﻿/* EndScoreDisplay links Unity UI Text in End Screen to display the
+ * score of the previous game along with the high score. Both are retrieved
+ * from player prefs through the custom PlayerPrefsManager. 
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScoreDisplay : MonoBehaviour {
+
+    public Text highScore;
+    public Text lastScore;
+
+	// Use this for initialization
+	void Start () {
+        highScore.text = "High Score: " + PlayerPrefsManager.GetHighScore().ToString();
+        lastScore.text = "Your Score: " + PlayerPrefsManager.GetLastScore().ToString();
+	}
+
+}
