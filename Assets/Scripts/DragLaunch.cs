bbb002b41fed44ef779ccf7b0aa212f7ef46903f
mikeohl <mikeohl@live.com>
2018-03-07 05:56:48 -0800

    Added protection for NaN on launch velocity, and added comments

--- Assets/Scripts/DragLaunch.cs
+++ Assets/Scripts/DragLaunch.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿/* DragLaunch calculates launch velocity from mouse drag on click
+ * and release. Ball position before launch can also be set. 
+ */
 
+using UnityEngine;
 
 [RequireComponent(typeof(Ball))]
 public class DragLaunch : MonoBehaviour {
@@ -13,13 +16,25 @@ public class DragLaunch : MonoBehaviour {
         ball = GetComponent<Ball>();
     }
 
+    // Position the ball across the lane (x-axis) before launch
+    public void MoveStart(float xNudge) {
+        if (!ball.BallInPlay() && ball.BallInLane()) {
+            ball.transform.Translate(new Vector3(xNudge, 0, 0));
+            if (!ball.BallInLane()) {
+                ball.transform.Translate(new Vector3(-xNudge, 0, 0));
+            }
+        }
+    }
+
     // Capture time and position of drag start
     public void DragStart () {
         startTime = Time.time;
         startPosition = Input.mousePosition;
     }
 
-    // Launch the ball
+    // Capture time and position of drag end and Launch the ball.
+    // Ball velocity is calculated based on drag distance over time.
+    // Launch velocity is clamped to 'reasonable' velocity
     public void DragEnd () {
         if (!ball.BallInPlay()) {
             endTime = Time.time;
@@ -30,22 +45,16 @@ public class DragLaunch : MonoBehaviour {
             launchVelocity.x = (endPosition.x - startPosition.x) / dragDuration / 5.0f;
             launchVelocity.z = (endPosition.y - startPosition.y) / dragDuration * 2.0f;
 
-            launchVelocity.z = Mathf.Clamp(launchVelocity.z, 300.0f, 1500.0f);
-            if (launchVelocity.x == double.NaN || launchVelocity.z == double.NaN) {
-                Debug.LogWarning("Launch velocity contained NaN");
+            // Clamp to 'reasonable' velocity
+            launchVelocity.z = Mathf.Clamp(launchVelocity.z, 300.0f, 1250.0f);
+
+            // Protect again NaN result. Only launch with valid velocity
+            if (launchVelocity.x != double.NaN && launchVelocity.z != double.NaN) {
+                ball.Launch(launchVelocity);
                 return;
             }
-            Debug.Log(launchVelocity.z);
-            ball.Launch(launchVelocity);
-        }
-    }
-
-    public void MoveStart (float xNudge) {
-        if (!ball.BallInPlay() && ball.BallInLane()) {
-            ball.transform.Translate(new Vector3(xNudge, 0, 0));
-            if (!ball.BallInLane()) {
-                ball.transform.Translate(new Vector3(-xNudge, 0, 0));
-            }
+            Debug.LogWarning("Launch velocity contained NaN");
+            return;
         }
     }
 }
